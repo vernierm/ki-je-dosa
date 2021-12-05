fe3dab6371df84ad9fcc435c95060c3d89d3ae23
vernierm <verniermt@gmail.com>
2021-12-05 21:43:39 +0100

    rewrite Form using hooks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 
 function InputLabel(props) {
@@ -17,87 +17,55 @@ function InputLabel(props) {
   );
 }
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      male: '',
-      female: '',
-      where: '',
-      doingWhat: '',
-      thirdPerson: '',
-      statement: '',
-    };
+const initialValues = {
+  male: '',
+  female: '',
+  where: '',
+  doingWhat: '',
+  thirdPerson: '',
+  statement: '',
+}
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+export default function Form(props) {
+  const [values, setValues] = useState(initialValues);
 
-  clearState() {
-    this.setState({
-      male: '',
-      female: '',
-      where: '',
-      doingWhat: '',
-      thirdPerson: '',
-      statement: '',
-    });
-  }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    })
+  };
 
-  handleChange(event) {
-    switch(event.target.name) {
-      case 'male':
-        this.setState({male: event.target.value});
-        break;
-      case 'female':
-        this.setState({female: event.target.value});
-        break;
-      case 'where':
-        this.setState({where: event.target.value});
-        break;
-      case 'doingWhat':
-        this.setState({doingWhat: event.target.value});
-        break;
-      case 'thirdPerson':
-        this.setState({thirdPerson: event.target.value});
-        break;
-      case 'statement':
-        this.setState({statement: event.target.value});
-        break;
-      default:
-        console.log('dev error');
-    }
-  }
+  const resetValues = () => {
+    setValues(initialValues);
+  };
 
-  isValid(value) {
-    return value.male && value.female && value.where && value.doingWhat && value.thirdPerson && value.statement;
-  }
+  const valid = (values) => {
+    return values.male && values.female && values.where && values.doingWhat && values.thirdPerson && values.statement;
+  };
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const currentState = this.state;
-    if (!this.isValid(currentState)) {
+    const currentValues = values;
+    if (!valid(currentValues)) {
       alert('popuni sva polja');
     } else {
       event.target.reset();
-      this.clearState();
-      this.props.onClick(currentState);
+      resetValues();
+      props.onClick(currentValues);
     }
   }
 
-  render() {
-    return (
-      <form className="Form-input" onSubmit={this.handleSubmit}>
-        <InputLabel label="Muški:" name="male" value={this.state.male} onChange={this.handleChange} />
-        <InputLabel label="Ženska:" name="female" value={this.state.female} onChange={this.handleChange} />
-        <InputLabel label="Kadi?" name="where" value={this.state.where} onChange={this.handleChange} />
-        <InputLabel label="Ča delaju?" name="doingWhat" value={this.state.doingWhat} onChange={this.handleChange} />
-        <InputLabel label="Ki je doša?" name="thirdPerson" value={this.state.thirdPerson} onChange={this.handleChange} />
-        <InputLabel label="Ča je reka?" name="statement" value={this.state.statement} onChange={this.handleChange} />
-        <input className="Form-button" type="submit" value="Unesi" />
-      </form>
-    );
-  }
+  return (
+    <form className="Form-input" onSubmit={handleSubmit}>
+      <InputLabel label="Muški:" name="male" value={values.male} onChange={handleInputChange} />
+      <InputLabel label="Ženska:" name="female" value={values.female} onChange={handleInputChange} />
+      <InputLabel label="Kadi?" name="where" value={values.where} onChange={handleInputChange} />
+      <InputLabel label="Ča delaju?" name="doingWhat" value={values.doingWhat} onChange={handleInputChange} />
+      <InputLabel label="Ki je doša?" name="thirdPerson" value={values.thirdPerson} onChange={handleInputChange} />
+      <InputLabel label="Ča je reka?" name="statement" value={values.statement} onChange={handleInputChange} />
+      <input className="Form-button" type="submit" value="Unesi" />
+    </form>
+  );
 }
-
-export default Form;
